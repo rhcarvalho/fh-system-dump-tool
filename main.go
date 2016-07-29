e18dbcd2a9f950cd8f41726378d8f71f27c351c7
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-07-29 11:14:33 +0200

    Extract loop body into a reusable function

--- main.go
+++ main.go
@@ -91,42 +91,12 @@ func resourceDefinitions(cmdFactory getProjectResourceCmdFactory, project string
 		// to different files without processing the contents of the
 		// output from oc.
 		for _, resource := range types {
-			var err error
-			var stdoutCloser, stderrCloser io.Closer
-
 			cmd := cmdFactory(project, resource)
-			cmd.Stdout, stdoutCloser, err = outFor(project, resource)
-			if err != nil {
+			if err := runCmdCaptureOutput(cmd, project, resource, outFor, errOutFor); err != nil {
+				// In case of errors, report it, skip the
+				// current resource type and proceed with the
+				// next.
 				errors = append(errors, err.Error())
-				// Since we couldn't get an io.Writer for
-				// cmd.Stdout, give up processing this resource
-				// type, and skip to the next type.
-				continue
-			}
-			defer stdoutCloser.Close()
-
-			var buf bytes.Buffer
-			cmd.Stderr, stderrCloser, err = errOutFor(project, resource)
-			if err != nil {
-				errors = append(errors, err.Error())
-				// We can possibly try to run the command
-				// without an io.Writer from errOutFor. In this
-				// case, we'll attach an in-memory buffer so
-				// that we can include the stderr output in
-				// errors.
-				cmd.Stderr = &buf
-			} else {
-				defer stderrCloser.Close()
-				// Send stderr to both the io.Writer from
-				// errOutFor, and an in-memory buffer, used to
-				// enrich error messages.
-				cmd.Stderr = io.MultiWriter(cmd.Stderr, &buf)
-			}
-
-			// TODO: limit the execution time with a timeout.
-			err = cmd.Run()
-			if err != nil {
-				errors = append(errors, fmt.Sprintf("command %q: %v: %v", strings.Join(cmd.Args, " "), err, buf.String()))
 				continue
 			}
 		}
@@ -135,6 +105,39 @@ func resourceDefinitions(cmdFactory getProjectResourceCmdFactory, project string
 		}
 		return nil
 	}
+}
+
+func runCmdCaptureOutput(cmd *exec.Cmd, project, resource string, outFor, errOutFor projectResourceWriterCloserFactory) error {
+	var err error
+	var stdoutCloser, stderrCloser io.Closer
+
+	cmd.Stdout, stdoutCloser, err = outFor(project, resource)
+	if err != nil {
+		// Since we couldn't get an io.Writer for cmd.Stdout, give up
+		// processing this resource type.
+		return err
+	}
+	defer stdoutCloser.Close()
+
+	var buf bytes.Buffer
+	cmd.Stderr, stderrCloser, err = errOutFor(project, resource)
+	if err != nil {
+		// We can possibly try to run the command without an io.Writer
+		// from errOutFor. In this case, we'll attach an in-memory
+		// buffer so that we can include the stderr output in errors.
+		cmd.Stderr = &buf
+	} else {
+		defer stderrCloser.Close()
+		// Send stderr to both the io.Writer from errOutFor, and an
+		// in-memory buffer, used to enrich error messages.
+		cmd.Stderr = io.MultiWriter(cmd.Stderr, &buf)
+	}
+
+	// TODO: limit the execution time with a timeout.
+	if err = cmd.Run(); err != nil {
+		return fmt.Errorf("command %q: %v: %v", strings.Join(cmd.Args, " "), err, buf.String())
+	}
+	return nil
 }
 
 // GetProjects returns a list of project names visible by the current logged in
