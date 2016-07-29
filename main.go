a4367bb8d303f18e506636e717aada091c2d0e83
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-07-29 15:50:05 +0200

    Move dump writers to a new file

--- main.go
+++ main.go
@@ -37,37 +37,6 @@ func (e errorList) Error() string {
 	return "multiple errors:\n" + strings.Join(e, "\n")
 }
 
-// A projectResourceWriterFactory generates io.Writers for dumping data of a
-// particular resource type within a project.
-type projectResourceWriterCloserFactory func(project, resource string) (io.Writer, io.Closer, error)
-
-// outToFile returns a function that creates an io.Writer that writes to a file
-// in basepath with extension, given a project and resource.
-func outToFile(basepath, extension string) projectResourceWriterCloserFactory {
-	return func(project, resource string) (io.Writer, io.Closer, error) {
-		projectpath := filepath.Join(basepath, "projects", project)
-		err := os.MkdirAll(projectpath, 0770)
-		if err != nil {
-			return nil, nil, err
-		}
-		f, err := os.Create(filepath.Join(projectpath, resource+"."+extension))
-		if err != nil {
-			return nil, nil, err
-		}
-		return f, f, nil
-	}
-}
-
-// OutToTGZ returns an anonymous factory function that will create an io.Writer which writes into the tar archive
-// provided. The path inside the tar.gz file is calculated from the project and resource provided
-func outToTGZ(extension string, tarFile *Archive) projectResourceWriterCloserFactory {
-	return func(project, resource string) (io.Writer, io.Closer, error) {
-		projectPath := filepath.Join("projects", project)
-		writer := tarFile.GetWriterToFile(filepath.Join(projectPath, resource+"."+extension))
-		return writer, writer, nil
-	}
-}
-
 func runCmdCaptureOutput(cmd *exec.Cmd, project, resource string, outFor, errOutFor projectResourceWriterCloserFactory) error {
 	var err error
 	var stdoutCloser, stderrCloser io.Closer
