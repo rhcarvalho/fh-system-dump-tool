b4f437d46041513a384125a63fbd55948ea6d11e
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-08-16 11:23:12 +0200

    Remove old dead code

--- errors.go
+++ errors.go
@@ -1,24 +1,5 @@
 package main
 
-import "strings"
-
-// An errorList accumulates multiple errors and implements error.
-type errorList []error
-
-func (e errorList) Error() string {
-	switch len(e) {
-	case 0:
-		return ""
-	case 1:
-		return e[0].Error()
-	}
-	var msgs = make([]string, len(e))
-	for i := range e {
-		msgs[i] = e[i].Error()
-	}
-	return "multiple errors:\n" + strings.Join(msgs, "\n")
-}
-
 // IgnorableError is an error that has an extra method to tell whether it should
 // be ignored. Ignored errors may be omitted from standard program output.
 type IgnorableError interface {
