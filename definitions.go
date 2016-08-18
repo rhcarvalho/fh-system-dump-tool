e8166dc216b313b5402e6e01bfe74e41833bab6b
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-08-18 14:38:38 +0200

    Move note to a more suitable place

--- definitions.go
+++ definitions.go
@@ -9,6 +9,10 @@ import (
 // GetResourceDefinitionTasks sends tasks to fetch the definitions of all
 // resources in all projects.
 func GetResourceDefinitionTasks(tasks chan<- Task, runner Runner, projects, resources []string) {
+	// NOTE: we could fetch all resources of all types in a single call to
+	// oc, by passing a comma-separated list of resource types. Instead, we
+	// call oc multiple times to send the output to different files without
+	// processing the contents of the output from oc.
 	for _, p := range projects {
 		for _, resource := range resources {
 			tasks <- ResourceDefinition(runner, p, resource)
@@ -20,11 +24,6 @@ func GetResourceDefinitionTasks(tasks chan<- Task, runner Runner, projects, reso
 // definition for the given resource in the given project.
 func ResourceDefinition(r Runner, project, resource string) Task {
 	return func() error {
-		// NOTE: we could fetch all resources of all types in a single
-		// call to oc, by passing a comma-separated list of resource
-		// types. Instead, we call oc multiple times to send the output
-		// to different files without processing the contents of the
-		// output from oc.
 		var args []string
 		if project != "" {
 			args = append(args, "-n", project)
