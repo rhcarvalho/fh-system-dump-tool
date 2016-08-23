c74319f7dffd5d85f31d0cfadca6a450475a0da5
Phil Brookes <phil.brookes@redhat.com>
2016-08-23 16:53:00 +0100

    check event log for errors, and improve tests on analysis - RHMAP-9453

--- analysis.go
+++ analysis.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"io/ioutil"
+	"os"
 	"path/filepath"
-	"strings"
 )
 
 type Info struct {
@@ -17,23 +15,27 @@ type Info struct {
 }
 
 type Result struct {
-	CheckName     string `json:"checkName" yaml:"checkName"`
-	Status        int    `json:"status" yaml:"status"`
-	StatusMessage string `json:"statusMessage" yaml:"statusMessage"`
-	Info          []Info `json:"info" yaml:"info"`
+	CheckName     string  `json:"checkName" yaml:"checkName"`
+	Status        int     `json:"status" yaml:"status"`
+	StatusMessage string  `json:"statusMessage" yaml:"statusMessage"`
+	Info          []Info  `json:"info" yaml:"info"`
+	Events        []Event `json:"events" yaml:"events"`
+}
+
+type Event struct {
+	Kind           string `json:"kind"`
+	InvolvedObject struct {
+		Namespace string `json:"namespace"`
+		Name      string `json:"name"`
+	} `json:"involvedObject"`
+	Reason  string `json:"reason"`
+	Message string `json:"message"`
+	Count   int    `json:"count"`
+	Type    string `json:"type"`
 }
 
 type Events struct {
-	Items []struct {
-		Kind           string `json:"kind"`
-		InvolvedObject struct {
-			Namespace string `json:"namespace"`
-			Name      string `json:"name"`
-		} `json:"involvedObject"`
-		Reason  string `json:"reason"`
-		Message string `json:"message"`
-		Count   int    `json:"count"`
-	} `json:"items"`
+	Items []Event `json:"items"`
 }
 
 type DeploymentConfigs struct {
@@ -49,22 +51,23 @@ type DeploymentConfigs struct {
 	} `json:"items"`
 }
 
-type CheckTask func(string, string) (Result, error)
+type CheckTask func(DumpedJSONResourceFactory) (Result, error)
 
 func GetAnalysisTasks(tasks chan<- Task, basepath string, projects []string, results chan<- CheckResults) {
 	// Platform-wide analysis goes here
 
 	// project specific analysis in here
 	for _, p := range projects {
-		tasks <- CheckProjectTask(p, basepath, results)
+		JSONResourceFactory := getDumpedJSONResourceFactory([]string{basepath, "projects", p})
+		tasks <- CheckProjectTask(p, results, JSONResourceFactory)
 	}
 }
 
 // CheckTasks is a task factory for tasks that diagnose system conditions.
-func CheckProjectTask(project, basepath string, results chan<- CheckResults) Task {
+func CheckProjectTask(project string, results chan<- CheckResults, JSONResourceFactory DumpedJSONResourceFactory) Task {
 	return checkProjectTask(func() []CheckTask {
-		return []CheckTask{CheckImagePullBackOff, CheckDeployConfigsReplicasNotZero}
-	}, project, basepath, results)
+		return []CheckTask{CheckEventLogForErrors, CheckDeployConfigsReplicasNotZero}
+	}, JSONResourceFactory, project, results)
 }
 
 // A getProjectCheckFactory generates tasks to diagnose system conditions.
@@ -78,14 +81,14 @@ type CheckResults struct {
 // checkTasks executes all the CheckTasks returned from the supplied
 // checkFactory against the specified project. The results of the checks are
 // combined into a single JSON object and returned
-func checkProjectTask(checkFactory getProjectCheckFactory, project, basepath string, results chan<- CheckResults) Task {
+func checkProjectTask(checkFactory getProjectCheckFactory, JSONResourceFactory DumpedJSONResourceFactory, project string, results chan<- CheckResults) Task {
 	return func() error {
 		result := CheckResults{Scope: project, Results: []Result{}}
 		checks := checkFactory()
 
 		var errors errorList
 		for _, check := range checks {
-			res, err := check(project, basepath)
+			res, err := check(JSONResourceFactory)
 			if err != nil {
 				errors = append(errors, err)
 			}
@@ -102,55 +105,58 @@ func checkProjectTask(checkFactory getProjectCheckFactory, project, basepath str
 	}
 }
 
-// getDumpedResourceAsStruct retrieves the requested resource from the dump directory and
-// decodes the JSON into the provided interface
-func getDumpedResource(path []string, dest interface{}) error {
-	filePath := filepath.Join(path...)
-	contents, err := ioutil.ReadFile(filePath)
+// Takes an array of strings describing the path to a JSON file
+// which will be parsed and loaded into the supplied interface
+type DumpedJSONResourceFactory func([]string, interface{}) error
 
-	decoder := json.NewDecoder(bytes.NewBuffer(contents))
-	err = decoder.Decode(&dest)
-	if err != nil {
-		return err
+// Returns a factory which will load resource from a given basepath. The factory parses the file
+// contents as JSON and loads it into the provided dest interface
+func getDumpedJSONResourceFactory(basepath []string) DumpedJSONResourceFactory {
+
+	return func(path []string, dest interface{}) error {
+		file := filepath.Join(append(basepath, path...)...)
+		contents, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		decoder := json.NewDecoder(contents)
+		if err := decoder.Decode(&dest); err != nil {
+			return err
+		}
+		return nil
 	}
 
-	return nil
 }
 
-// CheckImagePullBackOff checks all events in the supplied project and if any
-// are exhibiting signs that they have experienced an ImagePullBackOff recently
-// this will be reflected in the returned Result data. Any errors are written to
-// the supplied stdErr writer.
-func CheckImagePullBackOff(project, basepath string) (Result, error) {
-	result := Result{Status: 0, StatusMessage: "this issue was not detected", CheckName: "check deploys for ImagePullBackOff error"}
+// CheckEventLogForErrors checks all events in the supplied project and if any
+// are not type 'Normal' (i.e. Warning or Error), it will add them to the returned results.
+func CheckEventLogForErrors(JSONResourceFactory DumpedJSONResourceFactory) (Result, error) {
+	result := Result{Status: 0, StatusMessage: "this issue was not detected", CheckName: "check eventlog for any errors", Info: []Info{}, Events: []Event{}}
 	events := Events{}
-	err := getDumpedResource([]string{basepath, "projects", project, "definitions", "events.json"}, &events)
-	if err != nil {
+	if err := JSONResourceFactory([]string{"definitions", "events.json"}, &events); err != nil {
 		result.Status = 2
 		result.StatusMessage = "Error executing task"
 		return result, err
 	}
 
 	for _, event := range events.Items {
-		if event.Reason == "FailedSync" && strings.Contains(event.Message, "ImagePullBackOff") {
-			info := Info{Name: event.InvolvedObject.Name, Namespace: event.InvolvedObject.Namespace, Kind: event.Kind, Count: event.Count, Message: event.Message}
+		if event.Type != "Normal" {
 			result.Status = 1
-			result.StatusMessage = "'ImagePullBackOff' error detected"
-			result.Info = append(result.Info, info)
+			result.StatusMessage = "Errors detected in event log"
+			result.Events = append(result.Events, event)
 		}
 	}
 
 	return result, nil
 }
 
-// CheckDeployConfigsReplicasNotZero checks all deployment configs in the
-// supplied project and if any have replicas set to zero this will be reflected
-// in the returned Result data. Any errors are written to the supplied stdErr
-// writer.
-func CheckDeployConfigsReplicasNotZero(project, basepath string) (Result, error) {
-	result := Result{Status: 0, StatusMessage: "this issue was not detected", CheckName: "check deployconfig replicas not 0"}
+// CheckDeployConfigsReplicasNotZero checks all deployment configs in the supplied
+// JSON Resource Factory, and if any are found with a replica of 0, it will add a
+// note about it to the returned result
+func CheckDeployConfigsReplicasNotZero(ResourceFactory DumpedJSONResourceFactory) (Result, error) {
+	result := Result{Status: 0, StatusMessage: "this issue was not detected", CheckName: "check deployconfig replicas not 0", Info: []Info{}, Events: []Event{}}
 	deploymentConfigs := DeploymentConfigs{}
-	err := getDumpedResource([]string{basepath, "projects", project, "definitions", "deploymentconfigs.json"}, &deploymentConfigs)
+	err := ResourceFactory([]string{"definitions", "deploymentconfigs.json"}, &deploymentConfigs)
 	if err != nil {
 		result.Status = 2
 		result.StatusMessage = "Error executing task"
